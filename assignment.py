da4eb0f5220507c03ae5995cd11870d9b907e449
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-11 20:11:21 +0545

    Typo Fix

--- assignment.py
+++ assignment.py
@@ -1,25 +1,28 @@
 import time
 visited_word = {}
 
-sentence_list = ["Hello World", "Hello Again", "Hello There", "Apple", "Anaconda"]
+sentence_list = ["Hello World", "Hello Again", "Hello There", "Apple", "Apple and Anaconda"]
 
 
 def get_sentences(word):
     if word in visited_word:
+        print("Already Visited Word. Fetch from database")
         return visited_word[word]
 
     # simulating a operation that takes a long time
     print("Performing search operation...")
     time.sleep(3)
 
-    sentence_list = []
+    sentences = []
     for sentence in sentence_list:
         if word in sentence:
-            sentence_list.append(sentence)
+            sentences.append(sentence)
 
-    visited_word[word] = sentence_list
-    return sentence_list
+    visited_word[word] = sentences
+    return sentences
 
 
 print(get_sentences("Hello"))
 print(get_sentences("Hello"))
+print(get_sentences("Apple"))
+print(get_sentences("Apple"))
