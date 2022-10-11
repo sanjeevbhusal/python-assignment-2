4893bd475ea117eae5e10fb15ecd59e3200710b7
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-11 17:25:21 +0545

    seperated validators for int, string and boolean

--- assignment.py
+++ assignment.py
@@ -1,34 +1,86 @@
-def validate(data_dict, validator_dict):
-    try:
-        for key, value in data_dict.items():
-            validators = validator_dict.get(key)
-            if not validators:
-                continue
+available_string_validators = ["sanjeev", "max_length"]
+available_integer_validators = ["min_value", "max_value"]
+available_boolean_validators = ["value"]
 
-            data_type_required = validators.get("data_type")
-            if data_type_required == str:
-                min_length_required = validators.get("min_length")
-                max_length_required = validators.get("max_length")
-                assert len(value) >= min_length_required
-                assert len(value) <= max_length_required
-            elif data_type_required == int:
-                min_value_required = validators.get("min_value")
-                max_value_required = validators.get("max_value")
-                assert value >= min_value_required
-                assert value <= max_value_required
-            elif data_type_required == bool:
-                value_required = validators.get("value")
-                assert value == value_required
+
+class IncorrectValidator(Exception):
+    pass
+
+
+def validate_string(string, validators):
+    try:
+        for validator in validators:
+            if validator not in available_string_validators:
+                raise IncorrectValidator
+
+            if validator == "min_length":
+                assert len(string) >= validators.get("min_length")
+
+            elif validator == "max_length":
+                assert len(string) <= validators.get("max_length")
     except AssertionError:
-        return False
-    else:
-        return True
+        raise AssertionError
+    except IncorrectValidator:
+        raise IncorrectValidator
+
+
+def validate_integer(integer, validators):
+    try:
+        for validator in validators:
+            if validator not in available_integer_validators:
+                raise IncorrectValidator
+
+            if validator == "min_value":
+                assert integer >= validators.get("min_value")
+
+            elif validator == "max_value":
+                assert integer <= validators.get("max_value")
+    except AssertionError:
+        raise AssertionError
+    except IncorrectValidator:
+        raise IncorrectValidator
+
+
+def validate_boolean(boolean, validators):
+    try:
+        for validator in validators:
+            if validator not in available_boolean_validators :
+                raise IncorrectValidator
+
+            if validator == "value":
+                assert boolean >= validators.get("value")
+    except AssertionError:
+        raise AssertionError
+    except IncorrectValidator:
+        raise IncorrectValidator
+
+
+def validate(data_dict, validator_dict):
+    for key, value in data_dict.items():
+        validators = validator_dict.get(key)
+        if not validators:
+            continue
+        try:
+            data_type_required = validators["data_type"]
+            del validators["data_type"]
+            assert type(value) == data_type_required
+            if data_type_required == str:
+                validate_string(value, validators)
+            elif data_type_required == int:
+                validate_integer(value, validators)
+            elif data_type_required == bool:
+                validate_boolean(value, validators)
+        except (AssertionError, IncorrectValidator, KeyError):
+            return False
+        else:
+            return True
 
 
 data = {"id": 1, "name": "Sanjeev", "is_female": False}
-validator = {"id": {"data_type": int, "min_value": 0, "max_value": 10},
+validator = {"id": {"data_type": int, "min_value": 1, "max_value": 10, },
              "name": {"data_type": str, "min_length": 1, "max_length": 100},
-             "is_female": {"data_type": bool, "value": False}}
+             "is_female": {"data_type": bool, "value": False}
+             }
 
 
 result = validate(data, validator)
