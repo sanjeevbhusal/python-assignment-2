a5960bc5e22b0d159151e00b46c979580931f217
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-12 10:58:52 +0545

    Used default dict instead of normal dict

--- assignment.py
+++ assignment.py
@@ -1,15 +1,12 @@
-visited_word = {}
+from collections import defaultdict
+visited_word = defaultdict(list)
 
-sentence_list = ["Hello World", "Hello Again", "Hello There", "Apple", "Apple and Anaconda"]
+sentence_list = ["Hello Hello", "Hello Again", "Hello There", "Apple", "Apple and Anaconda"]
 
 for sentence in sentence_list:
     word_list = sentence.split()
     for word in word_list:
-        if word in visited_word:
-            associated_sentences = visited_word[word]
-            associated_sentences.append(sentence)
-        else:
-            visited_word[word] = [sentence]
+        visited_word[word].append(sentence)
 
 
 def get_sentences(word):
@@ -20,4 +17,3 @@ print(get_sentences("Hello"))
 print(get_sentences("Hello"))
 print(get_sentences("Apple"))
 print(get_sentences("Apple"))
-
